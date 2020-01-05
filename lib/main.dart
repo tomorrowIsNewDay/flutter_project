11f0d84b64445fedcf537e0271c93db2be5dfaf3
lee_ming <602387427@qq.com>
2020-01-05 22:28:54 +0800

    :fire: init project

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+/**
+ * 《Flutter从入门到进阶-实战携程网App》
+ * 课程地址：
+ * https://coding.imooc.com/class/321.html
+ * 课程代码、文档：
+ * https://git.imooc.com/coding-321/
+ * 课程辅导答疑区：
+ * http://coding.imooc.com/learn/qa/321.html
+ */
+
+import 'package:flutter/material.dart';
+import 'package:flutter_trip/navigator/tab_navigator.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: TabNavigator(),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,100 @@
+/**
+ * 《Flutter从入门到进阶-实战携程网App》
+ * 课程地址：
+ * https://coding.imooc.com/class/321.html
+ * 课程代码、文档：
+ * https://git.imooc.com/coding-321/
+ * 课程辅导答疑区：
+ * http://coding.imooc.com/learn/qa/321.html
+ */
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+const APPBAR_SCROLL_OFFSET = 100;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List _imageUrls = [
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
+    'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
+  ];
+  double appBarAlpha = 0;
+
+  _onScroll(offset) {
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                if (scrollNotification is ScrollUpdateNotification &&
+                    scrollNotification.depth == 0) {
+                  //滚动且是列表滚动的时候
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _imageUrls.length,
+                      autoplay: true,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _imageUrls[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                      pagination: SwiperPagination(),
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text('哈哈'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text('首页'),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
